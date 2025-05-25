a073142a5af576798afc75e0249e3ae583c26941
ParthGiriGoswami <143078824+ParthGiriGoswami@users.noreply.github.com>
2025-05-25 21:46:53 +0530

    Update Createbutton.py

--- src/Screen/Createbutton.py
+++ src/Screen/Createbutton.py
@@ -1,18 +1,11 @@
 import flet as ft
-def create_custom_button(page,label, description,icon=None,on_click=None,h=105):
+def create_custom_button(page,label,description,icon=None,on_click=None,h=105):
     return ft.TextButton(
         content=ft.Row(
             [
-                ft.Container(
-                    content=ft.Icon(icon,size=100,color=ft.Colors.WHITE),
-                    alignment=ft.alignment.center_left,
-                    width=100,
-                ),
+                ft.Container(content=ft.Icon(icon,size=100,color=ft.Colors.WHITE),alignment=ft.alignment.center_left,width=100),
                 ft.Column(
-                    [
-                        ft.Text(value=label, size=30,color=ft.Colors.WHITE),
-                        ft.Text(value=description,color=ft.Colors.WHITE),
-                    ],
+                    [ft.Text(value=label, size=30,color=ft.Colors.WHITE),ft.Text(value=description,color=ft.Colors.WHITE)],
                     alignment=ft.MainAxisAlignment.START,
                 ),
             ],
