57cac95ccac5e51ff77c7ea0916b003bee2beb0d
ParthGiriGoswami <143078824+ParthGiriGoswami@users.noreply.github.com>
2025-05-25 21:47:11 +0530

    Update Mainpage.py

--- src/Screen/Mainpage.py
+++ src/Screen/Mainpage.py
@@ -27,15 +27,15 @@ def get_drives(file):
 def MainPage(page: ft.Page):
     global quickfiles
     yara_rule = """
-    rule ExampleMalware {
+         rule MalwareDetection {
         strings:
             $ransomware = {50 53 51 52 56 57 55 41 54 41 55 41 56 41 57}
             $keylogger = {6A 00 68 00 30 00 00 64 FF 35 30 00 00 00}
+            $shellcode = {31 C0 50 68 2E 65 78 65 68 63 61 6C 63 54 5F 50 57 56 50 FF D0}
             $cmd = "cmd.exe /c"
             $ps = "powershell.exe -nop -w hidden"
-            $shellcode = {31 C0 50 68 2E 65 78 65 68 63 61 6C 63 54 5F 50 57 56 50 FF D0}
         condition:
-            any of them
+            filesize < 2MB and (1 of ($ransomware, $keylogger, $shellcode) and 1 of ($cmd, $ps))
     }
     """
     compiled_rule = yara.compile(source=yara_rule)
